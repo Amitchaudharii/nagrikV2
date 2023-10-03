25815e8454d6434eaf97a290f302ffb3114377fd
amit chaudhary <chaudhary.amit2058@gmail.com>
2023-10-04 01:31:15 +0545

    Create detail page

--- js/main.js
+++ js/main.js
@@ -85,11 +85,11 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $(".slider").slick({
-    slidesToShow: 4, // Number of slides to show at a time
-    slidesToScroll: 1, // Number of slides to scroll on each navigation
-    autoplay: true, // Autoplay the slider
-    autoplaySpeed: 2000, // Autoplay speed in milliseconds (2 seconds)
-    arrows: true, // Display navigation arrows
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
     responsive: [
       {
         breakpoint: 991,
@@ -111,7 +111,6 @@ $(document).ready(function () {
       },
     ],
   });
-  // Add custom click event handlers for the arrow buttons
   $(".arrow-left").click(function () {
     $(".slider").slick("slickPrev");
   });
@@ -122,3 +121,44 @@ $(document).ready(function () {
 });
 
 // SLIDER END
+
+// DETAIL PAGE SLIDER
+
+$(document).ready(function () {
+  $(".slider-may-like").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".prev-button").click(function () {
+    $(".slider-may-like").slick("slickPrev");
+  });
+
+  $(".next-button").click(function () {
+    $(".slider-may-like").slick("slickNext");
+  });
+});
+
+// DETAIL PAGE SLIDER END
